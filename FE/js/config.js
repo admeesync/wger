f35f62cf1ec76e2105c07b86060eafb530f2cd57
Divyansh575 <divyanshsingh.ds575@gmail.com>
2026-07-19 23:26:26 +0530

    Public ip

--- FE/js/config.js
+++ FE/js/config.js
@@ -1,2 +1,2 @@
 // Only line you need to change when deploying FE to a different server than BE.
-const API_BASE = 'http://192.168.1.5:9000/api';
+const API_BASE = 'https://wger-h9qn.onrender.com/api';
