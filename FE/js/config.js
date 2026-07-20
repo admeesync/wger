ab805be445c597f79bee66e0e2cbb1583b0fddc2
Admeesync <development.admeesync@gmail.com>
2026-07-20 21:52:37 +0530

    Update config.js

--- FE/js/config.js
+++ FE/js/config.js
@@ -1,2 +1,2 @@
 // Only line you need to change when deploying FE to a different server than BE.
-const API_BASE = 'https://wger-h9qn.onrender.com/api';
+const API_BASE = 'https://wger-2.onrender.com/api';
